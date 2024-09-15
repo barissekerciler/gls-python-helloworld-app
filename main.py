9618410218db82b848f5932d2c37e04d8e18b194
baris <sekercilerbaris@gmail.com>
2024-09-16 00:26:44 +0200

    remove staging

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "[Staging] Hello, World!"
+    return "Hello, World!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
